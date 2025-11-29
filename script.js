0fcb2ca433af3498ec2987ee278b60bcaaa39efb
MunchDuster <72060614+MunchDuster@users.noreply.github.com>
2025-11-29 18:01:55 +1300

    make onvoiceschanged update the voices list and prevent duoble-initialisation

--- script.js
+++ script.js
@@ -6,19 +6,17 @@ const pitchSpeak = document.querySelector('#pitch');
 const volSpeak = document.querySelector('#vol');
 const rateSpeak = document.querySelector('#rate');
 const synth = window.speechSynthesis;
-const voices = synth.getVoices();
 
-// sort by name alphabetically
-voices.sort((a,b) => a.name.localeCompare(b.name))
+let voices;
+let voicesMap;
+let voice;
 
-// to make filtering more performant, use a map which has the name which is the filter key
-// (some OS&browser pairs result in 13000+ voices)
-const voicesMap = new Map();
-for (let voice of voices) {
-	voicesMap.set(voice.name.trim(), voice);
+if (speechSynthesis !== undefined) {
+	speechSynthesis.onvoiceschanged = setupVoices;
 }
-
-var voice;
+else {
+	setupVoices();
+}
 
 btnSpeak.addEventListener('click', () => {
 	var toSpeak = new SpeechSynthesisUtterance(txtInput.value);
@@ -29,11 +27,6 @@ btnSpeak.addEventListener('click', () => {
 	synth.cancel();
 	synth.speak(toSpeak);
 });
-
-PopulateVoices();
-if (speechSynthesis !== undefined) {
-	speechSynthesis.onvoiceschanged = applyFilter;
-}
 
 voiceList.onchange = () => {
 	const key = voiceList.value;
@@ -53,13 +46,18 @@ function PopulateVoices() {
 		voiceList.appendChild(listItem);
 	}
 }
-function applyFilter() {
-	ClearVoices();
-	PopulateVoices();
-}
-function ClearVoices() {
-	const children = voiceList.children.length;
-	for (let i = 0; i < children; i++) {
-		voiceList.removeChild(voiceList.children[0]);
+function setupVoices() {
+	voices = synth.getVoices(); // update list 
+	
+	// sort by name alphabetically
+	voices.sort((a,b) => a.name.localeCompare(b.name))
+	
+	// to make filtering more performant, use a map which has the name which is the filter key
+	// (some OS&browser pairs result in 13000+ voices)
+	voicesMap = new Map();
+	for (let voice of voices) {
+		voicesMap.set(voice.name.trim(), voice);
 	}
+
+	PopulateVoices();
 }
